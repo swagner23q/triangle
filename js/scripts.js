ff4a9234ae8c29afe34c27682cd8795bc37a055e
Kate Hall and Sara Wagner <kwarrenhall@gmail.com>
2016-01-19 14:25:13 -0800

    setup behaviorgrid, isosolese spec and script

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var triangle = function(shape) {
+  if (shape === 4, 4, 4) {
+  return true;
+  }
+};
--- specs/specs.js
+++ specs/specs.js
@@ -1,26 +1,10 @@
-describe('leapYear', function() {
-  it("is false for a year that is not divisible by 4, 100, or 400", function() {
-    expect(leapYear(1993)).to.equal(false);
+describe('triangle', function() {
+  it("is true when all sides are equal", function() {
+    expect(triangle("equilateral")).to.equal(true);
   });
 
-  it("is true for years divisible by 4", function() {
-    expect(leapYear(2004)).to.equal(true);
-  });
-
-  it("is false for years divisible by 100", function() {
-    expect(leapYear(1900)).to.equal(false);
-  });
-
-  it("is true for years divisible by 400", function () {
-    expect(leapYear(2000)).to.equal(true);
-  });
-
-  it("is false for words", function () {
-    expect(leapYear("september")).to.equal(false);
-  })
-
-  it("is false for not a number", function() {
-    expect(leapYear(NaN)).to.equal(false);
-  })
+it("is true if two sides are equal", function() {
+  expect(triangle("isosceles")).to.equal(true);
+});
 
 });
