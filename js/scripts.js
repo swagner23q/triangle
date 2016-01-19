37f9fd3cf961ba4b96c9adc5e0019d29eaadc4f2
Kate Hall and Sara Wagner <kwarrenhall@gmail.com>
2016-01-19 15:13:21 -0800

    add none of the above are true spec and script

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 var triangle = function(side1, side2, side3) {
   if (side1 === side2 && side2 === side3) {
   return true;
-  }
+} else if (side1 === side2 && side1 !== side3) {
+  return true;
+} else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
+  return true;
+} else if ((side1 + side2) < side3) {
+  return false;
+} else {
+  return false;
+}
 };
--- specs/specs.js
+++ specs/specs.js
@@ -6,4 +6,12 @@ describe('triangle', function() {
   it("is true if two sides are equal", function() {
   expect(triangle(2,2,3)).to.equal(true);
   });
+
+  it("is true is no sides are equal", function() {
+    expect(triangle(1,2,3)).to.equal(true);
+  });
+
+  it("is false when none above are met", function() {
+    expect(triangle(2,2,400)).to.equal(true);
+  });
 });
