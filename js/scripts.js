625cad650dd7d253dfe67b3481eedcdaf7b24657
Kate Hall and Sara Wagner <sara.wagner23q@gmail.com>
2016-01-19 16:29:24 -0800

    add final else if statements/alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
-var triangle = function(side1, side2, side3) {
-  if (side1 === side2 && side2 === side3) {
-  return true;
-} else if (side1 === side2 && side1 !== side3) {
-  return true;
-} else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
-  return true;
-} else if ((side1 + side2) < side3) {
-  return false;
-} else {
-  return false;
-}
-};
+$(document).ready(function() {
+  $("button#buttonSubmit").click(function(event) {
+      var side1 = $("select#side1").val();
+      var side2 = $("select#side2").val();
+      var side3 = $("select#side3").val();
+
+      if (side1 === side2 && side2 === side3) {
+        $('#equilateral').toggle();
+      } else if (side1 === side2 && side1 !== side3) {
+        $('#isosceles').toggle();
+      } else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
+        $('#scalene').toggle();
+      } else {
+        alert("Grats, you didn't make a triangle")
+        $('#notTriangle').toggle();
+        }
+  // var triangle = function(side1, side2, side3) {
+  //   if (side1 === side2 && side2 === side3) {
+  //   return true;
+  //   } else if (side1 === side2 && side1 !== side3) {
+  //     return true;
+  //   } else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
+  //     return true;
+  //   } else if ((side1 + side2) < side3) {
+  //     return false;
+  //   } else {
+  //     return false;
+  //   }
+    event.preventDefault();
+  });
+});
